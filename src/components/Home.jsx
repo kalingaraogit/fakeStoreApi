858967f16c36bb0710acbce38555c0f55b28ad94
kali <oborelee6@gmail.com>
2024-09-10 07:49:41 +0530

    three

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,8 +10,8 @@ const Home = () => {
   <div className="card-img-overlay d-flex flex-column
    justify-content-center">
     <div className="container">
-    <h5 className="card-title display-3 fw-bolder mb-0 "> NEW SEASON ARRIVALS. </h5>
-    <p className="card-text lead fs-2 ">CHECK OUT ALL THE   TRENDS </p>
+    <h5 className="card-title display-2 fw-bolder mb-0 "> NEW SEASON ARRIVALS. </h5>
+    <p className="card-text lead fs-2 ">CHECK OUT ALL THE   TRENDS. </p>
     </div>
   </div>
 </div>
--- src/redux/reducer/index.js
+++ src/redux/reducer/index.js
@@ -3,7 +3,7 @@ import handleCart from './handleCart'
 import {combineReducers} from "redux";
 
 const rootReducers = combineReducers({
-    handleCart,
+    handleCart ,
 })
 
 export default rootReducers;
